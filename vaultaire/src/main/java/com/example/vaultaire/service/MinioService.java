be541a7399d083917c5c692e60f335bfc3c99458
Himneesh-Kalra <kalrahimneesh04@gmail.com>
2026-05-08 18:58:09 +0530

    env vars for minio config

--- vaultaire/src/main/java/com/example/vaultaire/service/MinioService.java
+++ vaultaire/src/main/java/com/example/vaultaire/service/MinioService.java
@@ -4,51 +4,59 @@ import io.minio.GetObjectArgs;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
 import io.minio.RemoveObjectArgs;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.io.InputStream;
 
 @Service
 public class MinioService {
     private final MinioClient minioClient;
+    private final String bucket;
 
-
-    public MinioService(){
-        this.minioClient= MinioClient.builder()
-                .endpoint("http://localhost:9000")
-                .credentials("minio", "minio123")
+    public MinioService(@Value("${minio.url}") String url,
+                        @Value("${minio.access-key}") String accessKey,
+                        @Value("${minio.secret-key}") String secretKey,
+                        @Value("${minio.bucket}") String bucket) {
+        this.minioClient = MinioClient.builder()
+                .endpoint(url)
+                .credentials(accessKey, secretKey)
                 .build();
+        this.bucket = bucket;
     }
 
     public void deleteFile(String objectName) {
         try {
             minioClient.removeObject(
                     RemoveObjectArgs.builder()
-                            .bucket("vaultaire")
+                            .bucket(bucket)
                             .object(objectName)
                             .build()
             );
         } catch (Exception e) {
+            e.printStackTrace();
             throw new RuntimeException("Failed to delete file from MinIO");
         }
     }
 
-    public String uploadFile(MultipartFile file,String objectName)throws Exception{
+    public String uploadFile(MultipartFile file, String objectName) throws Exception {
         minioClient.putObject(
                 PutObjectArgs.builder()
-                        .bucket("vaultaire")
+                        .bucket(bucket)
                         .object(objectName)
-                        .stream(file.getInputStream(),file.getSize(),-1)
+                        .stream(file.getInputStream(), file.getSize(), -1)
                         .contentType(file.getContentType())
                         .build()
         );
         return objectName;
     }
+
     public InputStream getFile(String objectName) throws Exception {
         return minioClient.getObject(
                 GetObjectArgs.builder()
-                        .bucket("vaultaire")
+                        .bucket(bucket)
                         .object(objectName)
                         .build()
         );
